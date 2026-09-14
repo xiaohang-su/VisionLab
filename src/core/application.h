edabee8ff477f5c064e6de92a2ad1dd21f1b759d
xiaohang-su <xiaohang114514@gmail.com>
2026-09-14 11:00:09 +0800

    refactor: Application uses ModuleManager for Runtime lifecycle management

--- src/core/application.h
+++ src/core/application.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "core/module_manager.h"
 #include "runtime/runtime.h"
 #include "logging/logger.h"
 #include "config/config.h"
@@ -23,6 +24,7 @@ private:
     config::Config config_;
     logging::Logger logger_;
     runtime::Runtime runtime_;
+    core::ModuleManager module_manager_;
 
     bool initialized_;
 };
