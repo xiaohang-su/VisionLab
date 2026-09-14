93decd2785920cfb5442692c073583c35e7429bf
xiaohang-su <xiaohang-su@users.noreply.github.com>
2026-09-15 00:09:53 +0800

    build: add MinGW cross-compile toolchain file

---
+++ cmake/mingw-cross.cmake
@@ -0,0 +1,18 @@
+# CMake toolchain for cross-compiling Windows EXE on Linux
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(TOOLCHAIN_PREFIX /home/user/tools/mingw64/bin/x86_64-w64-mingw32)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+set(CMAKE_FIND_ROOT_PATH /home/user/tools/mingw64)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Static link everything for single-file EXE
+set(CMAKE_EXE_LINKER_FLAGS "-static -static-libgcc -static-libstdc++" CACHE STRING "" FORCE)
